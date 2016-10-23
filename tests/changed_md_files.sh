f65ea02dd651b40c41a629ec5aaf3759a7fc0123
hugovk <hugovk@users.noreply.github.com>
2016-10-23 12:25:26 +0300

    Check TRAVIS_PULL_REQUEST

--- tests/changed_md_files.sh
+++ tests/changed_md_files.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-if [ `git symbolic-ref --short HEAD` = "master" ]
+
+# TRAVIS_PULL_REQUEST: The pull request number if the current job
+# is a pull request, “false” if it’s not a pull request.
+
+# If not pull request
+if [[ $TRAVIS_PULL_REQUEST == 'false' ]]
 then
   # Get list of .md fies changed since previous commit
   changed_files=`git diff --name-only HEAD~1`
-else
+else # this is a PR
   # Get list of .md fies changed compared to master
   changed_files=`git diff --name-only master`
 fi
